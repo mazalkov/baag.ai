375da75f3a9cb1e9ebf727930c20a8bd0d6869fe
Andrew <32670729+anzdrew@users.noreply.github.com>
2023-11-11 09:59:08 +0000

    Bow to demands of linter

--- src/baag/main.py
+++ src/baag/main.py
@@ -9,19 +9,23 @@ st.title("Welcome to baag.ai!")
 st.subheader("Revolutionizing RFP Processes for Businesses")
 
 # Introduction
-st.markdown("""
+st.markdown(
+    """
     **baag.ai** is designed to simplify and streamline the process of filling out Requests for Proposals (RFPs). 
     Our tool uses AI to reduce the friction and pain points typically associated with RFPs, helping companies save time and resources.
-""")
+"""
+)
 
 # Features
 st.header("Features")
-st.markdown("""
+st.markdown(
+    """
 - **AI-Powered Assistance**: Leverage advanced AI to quickly fill out RFPs.
 - **Template Library**: Access a wide range of customizable RFP templates.
 - **Collaboration Tools**: Collaborate seamlessly with your team.
 - **Analytics and Insights**: Gain insights to improve your RFP responses.
-""")
+"""
+)
 
 # Testimonials (if available)
 st.header("What Our Users Say")
@@ -29,10 +33,12 @@ st.info("User testimonials and feedback can be showcased here.")
 
 # Call to Action
 st.header("Get Started with baag.ai")
-st.markdown("""
+st.markdown(
+    """
 Interested in transforming your RFP process? 
 [Sign up now](#) for early access or [contact us](#) for more information.
-""")
+"""
+)
 
 # Footer
 st.markdown("---")
